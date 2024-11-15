b72829501001289eae73372dadf5fbabaf9279ac
Yaswanth <vepanjeriyaswanth22@gmail.com>
2024-11-15 11:09:35 +0530

    This is my day one

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import './styles.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MovieGrid from './components/MoviesGrid';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <div className='container'>
+        <Header></Header>
+        <MovieGrid></MovieGrid>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
